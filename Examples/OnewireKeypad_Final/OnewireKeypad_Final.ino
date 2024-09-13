7f934c79584ee1db5c813f69f321d59221d63978
AndrewMascolo <Adeathsk8r@comcast.net>
2024-09-12 21:44:26 -0400

    Update OnewireKeypad_Final.ino
    Cleaned up the code

--- Examples/OnewireKeypad_Final/OnewireKeypad_Final.ino
+++ Examples/OnewireKeypad_Final/OnewireKeypad_Final.ino
@@ -1,7 +1,3 @@
-// Creator: Andrew Mascolo
-//
-//#include <Wire.h>
-//#include <LiquidCrystal_I2C.h>
 #include <OnewireKeypad.h>
 
 char KEYS[] = {
@@ -11,39 +7,26 @@ char KEYS[] = {
   '*', '0', '#', 'D'
 };
 
-//LiquidCrystal_I2C Lcd(0x20,20,4);
-//OnewireKeypad <LiquidCrystal_I2C, 12> KP( Lcd, KEYS, 4, 3, A0, 4700, 1000 );
-
-OnewireKeypad <Print, 16 > KP2(Serial, KEYS, 4, 4, A0, 4700, 1000 );
+OnewireKeypad <Print, 16 > myKeypad(Serial, KEYS, 4, 4, A0, 4700, 1000 );
 
 void setup () {
   Serial.begin(115200);
-
-  //  Lcd.init();
-  //  Lcd.backlight();
-
-  // This method is set in the constructor with a default value of 5.0
-  // You only need to include this method if your Arduino is not supplying 5v to
-  // the keypad. ie. ~4.7v or even with 3.3v Arduino boards too.
-  //KP.SetKeypadVoltage(5.0);
-  KP2.SetKeypadVoltage(5.0);
+  pinMode(13, OUTPUT);
+  myKeypad.setDebounceTime(50);
+  myKeypad.showRange();
 }
 
 void loop() {
-  //  if( KP.Getkey() )
-  //  {
-  //    Lcd.clear();
-  //    Lcd.home();
-  //    Lcd.print( F( "KP: " ) );
-  //    Lcd.setCursor(4,0);
-  //    KP.LatchKey();
-  //  }
 
-  if ( char key = KP2.Getkey() ) {
-    Serial << "Key: " << key << " State: ";
-    switch (KP2.Key_State()) {
+  if ( char key = myKeypad.getkey() ) {
+    Serial.println(key);
+    
+    digitalWrite(13, key == 'C'); // If key pressed is C, turn on LED, anything else will turn it off.
+    
+    switch (myKeypad.keyState()) {
       case PRESSED:
         Serial.println("PRESSED");
+        Serial.println(analogRead(4));
         break;
 
       case RELEASED:
@@ -56,5 +39,3 @@ void loop() {
     }
   }
 }
-
-
